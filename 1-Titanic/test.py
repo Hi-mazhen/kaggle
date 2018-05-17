3cc77cd62478ee2723a7fb2e1b78a570ca976ca3
mzh <mzh@771.com>
2018-05-17 16:41:03 +0800

    Titanic ex

---
+++ 1-Titanic/test.py
@@ -0,0 +1,14 @@
+# import matplotlib.pyplot as plt 
+# plt.plot([1,2,3]) 
+# plt.ylabel('some numbers') 
+# plt.show()
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+data_train = pd.read_csv("train.csv")
+data_train
+data_train.info()
+data_train.describe()
+
